8e74b216d437b1f1fbf34c04af9e71d1ce458a67
Raviraj Mahamuni <mahamuniraviraj@gmail.com>
2018-08-09 08:42:35 +0530

    Created First Angular App

---
+++ 01_GettingStarted/js/app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+(function(){
+    'use strict';
+    var myApp = angular.module('myApp',[]);
+   
+    myApp.controller('ToDoCtrl',function ($scope) {
+        
+        // Scope variables
+        $scope.total =5;
+        
+        
+        
+    });
+
+}());
+
+
+
